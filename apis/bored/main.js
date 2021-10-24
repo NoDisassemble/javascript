4bc1648fed7abccf62a31c3c96645186c49de5c9
NoDisassemble <anything@dant35an0nym0u5.com>
2021-10-24 16:33:31 -0700

    Add files via upload

---
+++ apis/bored/main.js
@@ -0,0 +1,29 @@
+const para = document.querySelector(".para");
+const btn = document.querySelector(".btn");
+
+const url = "http://www.boredapi.com/api/activity/";
+
+
+// Show loading spinner
+function showLoading() {
+    loading = document.querySelector(".loading");
+    document.querySelector(".para").innerHTML = "";
+    if (loading.style.display === "none") {
+        loading.style.display = "block";
+    } else {
+        loading.style.display = "none";
+    }
+}
+showLoading();
+
+async function boredAPI() {
+    showLoading();
+    response = await fetch(url);
+    data = await response.json();
+    showLoading();
+    para.textContent = `${data.activity}.`
+}
+
+btn.addEventListener('click', boredAPI)
+
+boredAPI();
