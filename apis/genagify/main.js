bb535b80faaa3156a2ff63c6234c5e056e98a874
NoDisassemble <anything@dant35an0nym0u5.com>
2021-10-25 20:13:31 -0700

    Add files via upload

---
+++ apis/genagify/main.js
@@ -0,0 +1,32 @@
+// API documentation can be found here: https://genderize.io/ & https://agify.io/
+
+// consts for IP info
+const name = document.querySelector(".name");
+const gender = document.querySelector(".gender");
+const probability = document.querySelector(".probability");
+const age = document.querySelector(".age");
+
+
+// get IP
+function getName(input) {
+    userName = input.value;
+    console.log(userName);
+    gen = `https://api.genderize.io?name=${userName}`;
+    console.log(gen);
+    userAge = `https://api.agify.io/?name=${userName}`;
+}
+
+async function getNameAndAge() {
+    response = await fetch(gen);
+    data = await response.json();
+    name.innerHTML = `Name: ${data.name}`;
+    gender.innerHTML = `Gender: ${data.gender}`;
+    probability.innerHTML = `Probability: ${data.probability}`;
+
+    response = await fetch(userAge);
+    data = await response.json();
+    age.innerHTML = `Age: ${data.age}`;
+
+
+    console.log(data)
+}
